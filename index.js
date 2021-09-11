0cec1d9c0683a1d4bb20886ea7eaba5ae1091b18
Juslin <komenan.komenan0@gmail.com>
2021-09-11 18:59:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const compression = require('compression');
+const logger = require('morgan');
+
+const app = express();
+
+/**
+    Middleware Configurations
+ */
+app.use(compression());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.get('/', (req, res) => {
+    res.send('Merci j\'ai bien reçu la demande et je suis content')
+})
+
+app.listen(3000, () => console.log('Serveur démarré avec succès.'))
